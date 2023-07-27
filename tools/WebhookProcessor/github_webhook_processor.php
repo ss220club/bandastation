bf7bbb39dce02e51024a8d0598152b83f7971472
larentoun <31931237+larentoun@users.noreply.github.com>
2023-07-27 20:57:56 +0900

    removes GBP

--- tools/WebhookProcessor/github_webhook_processor.php
+++ tools/WebhookProcessor/github_webhook_processor.php
@@ -282,7 +282,7 @@ function tag_pr($payload, $opened) {
 			$tags[] = $tag;
 
 	check_tag_and_replace($payload, '[dnm]', 'Do Not Merge', $tags);
-	check_tag_and_replace($payload, '[no gbp]', 'GBP: No Update', $tags);
+	// check_tag_and_replace($payload, '[no gbp]', 'GBP: No Update', $tags); // BANDASTATION EDIT REMOVAL - No GBP
 	check_tag_and_replace($payload, '[april fools]', 'April Fools', $tags);
 
 	return array($tags, $remove);
