d199b41fc5243ef99c32faab2d46df509be333ea
Furior <furiorg@gmail.com>
2024-04-11 02:52:43 +0700

    asdasfsdfgsdfgvdsf

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -8,6 +8,6 @@ wget -nv -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/relea
 chmod +x ~/.byond/bin/librust_g.so
 ldd ~/.byond/bin/librust_g.so
 
-wget -nv -O ~/.byond/bin/librust_g_ss220.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION_SS220/librust_g.so"
+wget -nv -O ~/.byond/bin/librust_g_ss220.so "https://github.com/ss220club/rust-g-tg/releases/download/$RUST_G_VERSION_SS220/librust_g.so"
 chmod +x ~/.byond/bin/librust_g_ss220.so
 ldd ~/.byond/bin/librust_g_ss220.so
