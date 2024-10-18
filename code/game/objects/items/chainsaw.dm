3bb0ba21b613be97a96996336a233ee1568e9bf9
larentoun <31931237+larentoun@users.noreply.github.com>
2024-10-18 15:01:47 +0300

    Fix translate 18.10.24 (#596)
    ## About The Pull Request
    Фикс переводов и дополнение перевода у "угрозы" агентов

--- code/game/objects/items/chainsaw.dm
+++ code/game/objects/items/chainsaw.dm
@@ -70,11 +70,11 @@
 
 /obj/item/chainsaw/suicide_act(mob/living/carbon/user)
 	if(!HAS_TRAIT(src, TRAIT_TRANSFORM_ACTIVE))
-		user.visible_message(span_suicide("[user] smashes [src] into [user.p_their()] neck, destroying [user.p_their()] esophagus! It looks like [user.p_theyre()] trying to commit suicide!"))
+		user.visible_message(span_suicide("[user] smashes [src] into [user.p_their()] neck, destroying [user.p_their()] esophagus! Кажется, [user.ru_p_they()] пытается совершить самоубийство!"))
 		playsound(src, 'sound/items/weapons/genhit1.ogg', 100, TRUE)
 		return BRUTELOSS
 
-	user.visible_message(span_suicide("[user] begins to tear [user.p_their()] head off with [src]! It looks like [user.p_theyre()] trying to commit suicide!"))
+	user.visible_message(span_suicide("[user] begins to tear [user.p_their()] head off with [src]! Кажется, [user.ru_p_they()] пытается совершить самоубийство!"))
 	playsound(src, 'sound/items/weapons/chainsawhit.ogg', 100, TRUE)
 	var/obj/item/bodypart/head/myhead = user.get_bodypart(BODY_ZONE_HEAD)
 	if(myhead)
--- tgui/packages/tgui/interfaces/Uplink/index.tsx
+++ tgui/packages/tgui/interfaces/Uplink/index.tsx
@@ -293,29 +293,29 @@ export class Uplink extends Component<{}, UplinkState> {
                         content={
                           <Box>
                             <Box>
-                              <Box>Your current level of threat.</Box> Threat
-                              determines
+                              <Box>Ваше текущее значение угрозы.</Box> Угроза
+                              влияет на
                               {has_objectives
-                                ? ' the severity of secondary objectives you get and '
+                                ? ' сложность получаемых второстепенных задач и '
                                 : ' '}
-                              what items you can purchase.&nbsp;
+                              список доступных для покупки предметов.&nbsp;
                               <Box mt={0.5}>
                                 {/* A minute in deciseconds */}
-                                Threat passively increases by{' '}
+                                Угроза пассивно увеличивается на{' '}
                                 <Box color="green" as="span">
                                   {calculateProgression(
                                     current_progression_scaling,
                                   )}
                                 </Box>
-                                &nbsp;every minute
+                                &nbsp;каждую минуту
                               </Box>
                               {Math.abs(progressionPercentage) > 0 && (
                                 <Box mt={0.5}>
-                                  Because your threat level is
+                                  Так как ваша угроза
                                   {progressionPercentage < 0
-                                    ? ' ahead '
-                                    : ' behind '}
-                                  of where it should be, you are getting
+                                    ? ' выше '
+                                    : ' ниже '}
+                                  той, на которой вы должны быть, вы получаете
                                   <Box
                                     as="span"
                                     color={
@@ -328,8 +328,10 @@ export class Uplink extends Component<{}, UplinkState> {
                                   >
                                     {progressionPercentage}%
                                   </Box>
-                                  {progressionPercentage < 0 ? 'less' : 'more'}{' '}
-                                  threat every minute
+                                  {progressionPercentage < 0
+                                    ? 'меньше'
+                                    : 'больше'}{' '}
+                                  угрозы каждую минуту
                                 </Box>
                               )}
                               {dangerLevelsTooltip}
@@ -355,7 +357,7 @@ export class Uplink extends Component<{}, UplinkState> {
                             selected={currentTab === 0}
                             onClick={() => this.setState({ currentTab: 0 })}
                           >
-                            Primary Objectives
+                            Основные задачи
                           </Tabs.Tab>
                         )}
                         {!!has_objectives && (
@@ -369,7 +371,7 @@ export class Uplink extends Component<{}, UplinkState> {
                             selected={currentTab === 1}
                             onClick={() => this.setState({ currentTab: 1 })}
                           >
-                            Secondary Objectives
+                            Вторичные задачи
                           </Tabs.Tab>
                         )}
                         <Tabs.Tab
@@ -398,7 +400,7 @@ export class Uplink extends Component<{}, UplinkState> {
                         px={2}
                         onClick={() => act('lock')}
                       >
-                        Lock
+                        Закрыть
                       </Button>
                     </Stack.Item>
                   )}
