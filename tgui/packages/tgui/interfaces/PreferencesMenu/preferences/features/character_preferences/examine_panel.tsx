0bf46099db4b7c5f7bfb04b5f56fd4c79d65b5e5
larentoun <31931237+larentoun@users.noreply.github.com>
2024-10-27 01:27:19 +0300

    it is translate in translate???

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/examine_panel.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/examine_panel.tsx
@@ -2,13 +2,13 @@ import { Feature } from '../base';
 import { FeatureTextInput } from '../base_bandastation';
 
 export const flavor_text: Feature<string> = {
-  name: 'Описание',
+  name: 'Flavor text',
   description: 'Опишите вашего персонажа!',
   component: FeatureTextInput,
 };
 
 export const silicon_flavor_text: Feature<string> = {
-  name: 'Описание (Silicon)',
+  name: 'Flavor text (Silicon)',
   description: 'Опишите вашего силикона!',
   component: FeatureTextInput,
 };
