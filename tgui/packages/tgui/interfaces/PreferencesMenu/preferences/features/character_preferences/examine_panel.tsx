d6c21fc76a7b54129c11efad4091211e64280c01
larentoun <31931237+larentoun@users.noreply.github.com>
2024-10-27 01:27:32 +0300

    typo

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/examine_panel.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/examine_panel.tsx
@@ -2,13 +2,13 @@ import { Feature } from '../base';
 import { FeatureTextInput } from '../base_bandastation';
 
 export const flavor_text: Feature<string> = {
-  name: 'Flavor text',
+  name: 'Flavor Text',
   description: 'Опишите вашего персонажа!',
   component: FeatureTextInput,
 };
 
 export const silicon_flavor_text: Feature<string> = {
-  name: 'Flavor text (Silicon)',
+  name: 'Flavor Text (Silicon)',
   description: 'Опишите вашего силикона!',
   component: FeatureTextInput,
 };
