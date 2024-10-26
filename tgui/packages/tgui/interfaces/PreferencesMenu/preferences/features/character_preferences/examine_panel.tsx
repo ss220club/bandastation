19d75404df5d2fed9583cfe7334e04d0766ce863
larentoun <31931237+larentoun@users.noreply.github.com>
2024-10-27 01:18:31 +0300

    update it

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/examine_panel.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/examine_panel.tsx
@@ -2,13 +2,13 @@ import { Feature } from '../base';
 import { FeatureTextInput } from '../base_bandastation';
 
 export const flavor_text: Feature<string> = {
-  name: 'Flavor Text',
+  name: 'Описание',
   description: 'Опишите вашего персонажа!',
   component: FeatureTextInput,
 };
 
 export const silicon_flavor_text: Feature<string> = {
-  name: 'Flavor Text (Silicon)',
+  name: 'Описание (Silicon)',
   description: 'Опишите вашего силикона!',
   component: FeatureTextInput,
 };
