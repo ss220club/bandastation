6438f871568038fa2ea199cbfe8c3918315787ba
larentoun <31931237+larentoun@users.noreply.github.com>
2024-11-19 16:58:32 +0300

    fix

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/hotkeys.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/hotkeys.tsx
@@ -1,7 +1,7 @@
 import { CheckboxInputInverse, FeatureToggle } from '../base';
 
 export const hotkeys: FeatureToggle = {
-  name: 'Классический горячие клавишы',
+  name: 'Классические горячие клавиши',
   category: 'ГЕЙМПЛЕЙ',
   description:
     'Возвращает к старым горячи клавишам, которые используют полосу ввода, а не всплывающие окна.',
